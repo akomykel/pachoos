a668812872f724ebcd00882b8e827cba1edb3d13
akomykel <akomykel@gmail.com>
2019-01-07 14:48:35 +0800

    Mykel: pachoos project

---
+++ src/main/java/com/project/pachoos/PachoosApplication.java
@@ -0,0 +1,14 @@
+package com.project.pachoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PachoosApplication.class, args);
+	}
+
+}
+
